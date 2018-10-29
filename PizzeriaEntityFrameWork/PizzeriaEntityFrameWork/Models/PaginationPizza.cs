0180653e9fbfd935644b9e6207954f76e2e32444
manuel910814 <manuel910814@outlook.es>
2018-10-29 21:03:59 +0100

    Listo_agregado!!!

---
+++ PizzeriaEntityFrameWork/PizzeriaEntityFrameWork/Models/PaginationPizza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PizzeriaEntityFrameWork.Models
+{
+    public  static class PaginationPizza
+    {
+
+        public static IEnumerable<Pizza> Page<Pizza>(this IEnumerable<Pizza> source)
+        {
+            var page = Convert.ToInt32(ConfigurationManager.AppSettings["page"]);
+            var pageSize= Convert.ToInt32(ConfigurationManager.AppSettings["pageSize"]);
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+            
+        }
+    }
+}
